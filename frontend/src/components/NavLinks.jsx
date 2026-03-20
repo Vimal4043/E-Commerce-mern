a3f0fcc798b6cee2df3e25b870123c2525a09ea0
Vimal <vimalkumar40437@gmail.com>
2026-03-20 13:50:51 +0530

    navlinks disabled

--- frontend/src/components/NavLinks.jsx
+++ frontend/src/components/NavLinks.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router";
 const NavLinks = ({ logout, closeMenu }) => {
   return (
     <>
-      <Link to="/" onClick={closeMenu} className="block px-4 py-2 hover:bg-gray-100">
+      <Link to="/" onClick={closeMenu} className="block px-4 py-2 disabled text-gray-400">
         Profile
       </Link>
 
-      <Link to="/" onClick={closeMenu} className="block px-4 py-2 hover:bg-gray-100">
+      <Link to="/" onClick={closeMenu} className="block px-4 py-2 disabled text-gray-400">
         Orders
       </Link>
 
